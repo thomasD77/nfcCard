4ec5a6073acabe0250c6d2f74ec783ebd85a14ce
thomasD77 <thomas.demeulenaere@hotmail.com>
2022-03-11 16:54:01 +0100

    archived account

--- resources/views/admin/members/index.blade.php
+++ resources/views/admin/members/index.blade.php
@@ -29,6 +29,7 @@
     <div class="bg-body-light">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center py-2">
+                @canany(['is_superAdmin', 'is_admin'])
                 <div class="flex-grow-1">
                     <h1 class="h3 fw-bold mb-2">
                         Members
@@ -44,6 +45,24 @@
                         </li>
                     </ol>
                 </nav>
+                @endcanany
+                @can('is_client')
+                    <div class="flex-grow-1">
+                        <h1 class="h3 fw-bold mb-2">
+                            Account
+                        </h1>
+                    </div>
+                    <nav class="flex-shrink-0 mt-3 mt-sm-0 ms-sm-3" aria-label="breadcrumb">
+                        <ol class="breadcrumb breadcrumb-alt">
+                            <li class="breadcrumb-item">
+                                <a class="link-fx" href="javascript:void(0)">DataTable</a>
+                            </li>
+                            <li class="breadcrumb-item" aria-current="page">
+                                settings
+                            </li>
+                        </ol>
+                    </nav>
+                @endcan
             </div>
         </div>
     </div>
