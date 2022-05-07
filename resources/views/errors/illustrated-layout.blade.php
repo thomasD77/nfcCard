96baacfdb0e4b2e52685786a27c4ae2bf7f48353
Thomas Demeulenaere <thomas.demeulenaere@hotmail.com>
2022-05-07 10:02:44 +0200

    fix error page

--- resources/views/errors/illustrated-layout.blade.php
+++ resources/views/errors/illustrated-layout.blade.php
@@ -470,9 +470,9 @@
                         @yield('message')
                     </p>
 
-                    <a href="{{ app('router')->has('home') ? route('home') : url('/') }}">
+                    <a href="{{ route('direction') }}">
                         <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
-                            {{ __('Go Home') }}
+                            {{ __('Go Back') }}
                         </button>
                     </a>
                 </div>
