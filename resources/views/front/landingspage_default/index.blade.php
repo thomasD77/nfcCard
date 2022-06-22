186cb114093fede8edb8a8fd50ce794749be2170
Ntriga.Agency <info@ntriga.agency>
2022-06-22 12:42:23 +0200

    test

--- resources/views/front/landingspage_default/index.blade.php
+++ resources/views/front/landingspage_default/index.blade.php
@@ -7,7 +7,9 @@
 
     <!-- Meta Data -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">--}}
     <meta name="format-detection" content="telephone=no"/>
     <meta name="format-detection" content="address=no"/>
     <meta name="author" content="SWAP" />
