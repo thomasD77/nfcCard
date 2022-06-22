30dff4e718e54d6ed9cf2d9cd05b93fc2aa6125f
Ntriga.Agency <info@ntriga.agency>
2022-06-22 14:30:04 +0200

    set

--- resources/views/emails/card-credentails.blade.php
+++ resources/views/emails/card-credentails.blade.php
@@ -39,7 +39,7 @@
 <div>
     @if($member['website'] != "")
         <a href="https://{{$member->website }}">
-            <img src="{{asset('images/socials/website.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/website.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
@@ -47,41 +47,41 @@
 <div>
     @if($member['facebook'] != "")
         <a href="{{ $member['facebook'] }}">
-            <img src="{{asset('images/socials/facebook.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/facebook.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
 <div>
     @if($member['instagram'] != "")
         <a href="{{ $member['instagram'] }}">
-            <img src="{{asset('images/socials/instagram.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/instagram.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
 <div>@if($member['linkedIn'] != "")
         <a href="{{ $member['linkedIn'] }}">
-            <img src="{{asset('images/socials/linkedin.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/linkedin.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
 <div>
     @if($member['twitter'] != "")
         <a href="{{ $member['twitter'] }}">
-            <img src="{{asset('images/socials/twitter.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/twitter.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
 <div>
     @if($member['tikTok'] != "")
         <a href="{{ $member['tikTok'] }}">
-            <img src="{{asset('images/socials/tiktok.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/tiktok.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
 <div>
     @if($member['whatsApp'] != "")
         <a href="https://wa.me/{{ $member->whatsApp }}">
-            <img src="{{asset('images/socials/whatsapp.png')}}" width="80" height="80" alt="">
+            <img src="{{asset('images/socials/whatsapp.png')}}" width="40" height="40" alt="">
         </a>
     @endif
 </div>
@@ -96,7 +96,7 @@
 
 
 
-<h3 style="margin-top: 80px; margin-bottom: 5px">Aarzel niet om mij te contacteren! </h3>
+<h3 style="margin-top: 40px; margin-bottom: 5px">Aarzel niet om mij te contacteren! </h3>
 <h3>Graag tot binnenkort. </h3>
 
 
