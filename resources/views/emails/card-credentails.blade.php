555cf5780ac9684a4ccc830c10128ba77e9a20bb
Ntriga.Agency <info@ntriga.agency>
2022-06-22 14:25:05 +0200

    test

--- resources/views/emails/card-credentails.blade.php
+++ resources/views/emails/card-credentails.blade.php
@@ -34,36 +34,41 @@
 <p>{{ $member['company'] }}</p>
 @endif
 
-<div>
-    @if($member['instagram'] != "")
-        <a href="{{ $member['instagram'] }}">
-            <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
-        </a>
-    @endif
-</div>
 
-<div>
-    @if($member['instagram'] != "")
-        <a href="{{ $member['instagram'] }}">
-            <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
-        </a>
-    @endif
-</div>
 
-<div>
-    @if($member['instagram'] != "")
-        <a href="{{ $member['instagram'] }}">
-            <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
-        </a>
-    @endif
-</div>
+<div class="row">
 
-<div>
-    @if($member['instagram'] != "")
-        <a href="{{ $member['instagram'] }}">
-            <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
-        </a>
-    @endif
+    <div class="column">
+        @if($member['instagram'] != "")
+            <a href="{{ $member['instagram'] }}">
+                <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
+            </a>
+        @endif
+    </div>
+
+    <div class="column">
+        @if($member['instagram'] != "")
+            <a href="{{ $member['instagram'] }}">
+                <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
+            </a>
+        @endif
+    </div>
+
+    <div class="column">
+        @if($member['instagram'] != "")
+            <a href="{{ $member['instagram'] }}">
+                <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
+            </a>
+        @endif
+    </div>
+
+    <div class="column">
+        @if($member['instagram'] != "")
+            <a href="{{ $member['instagram'] }}">
+                <img src="{{asset('images/socials/instagram.png')}}" width="50" height="50" alt="">
+            </a>
+        @endif
+    </div>
 </div>
 
 
