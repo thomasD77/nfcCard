08d966a2dadd1302be1688db9d6bd91d2b368c61
Ntriga.Agency <info@ntriga.agency>
2022-07-27 10:06:16 +0200

    pagination

--- resources/views/livewire/members-generator-detail.blade.php
+++ resources/views/livewire/members-generator-detail.blade.php
@@ -10,6 +10,10 @@
                     <option selected value="25">25</option>
                     <option value="50">50</option>
                     <option value="100">100</option>
+                    <option value="150">150</option>
+                    <option value="200">200</option>
+                    <option value="250">250</option>
+                    <option value="500">500</option>
                 </select>
                 <!-- End Pagination -->
 
