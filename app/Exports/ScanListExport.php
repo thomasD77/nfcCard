5d28896364ca522f6d7d9b285be5edcbccd7d75c
Ntriga.Agency <info@ntriga.agency>
2022-10-11 12:41:32 +0200

    admin export fix

--- app/Exports/ScanListExport.php
+++ app/Exports/ScanListExport.php
@@ -3,6 +3,8 @@
 namespace App\Exports;
 
 use App\Models\Contact;
+use App\Models\Member;
+use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
 class ScanListExport implements FromCollection
@@ -13,8 +15,12 @@ class ScanListExport implements FromCollection
     public function collection()
     {
         //
-        $contacts = Contact::where('archived', 0)
-            ->select('id', 'name', 'email', 'phone')
+        $member = Member::findOrFail(Auth::user()->member_id);
+
+        $contacts = Contact::with(['member'])
+            ->where('member_id', $member->id)
+            ->where('archived', 0)
+            ->select('id', 'name', 'email', 'phone', 'notes')
             ->get();
 
         return $contacts;
