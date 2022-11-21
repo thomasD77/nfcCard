0ce4a6d1ccb30adcadf73f2972ef0dba2ebf7b13
Ntriga.Agency <info@ntriga.agency>
2022-11-21 21:37:08 +0100

    flare error fix

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -24,23 +24,25 @@
                 <!-- Check if user trial is expired -->
                 @if(Auth::user()->member)
                     <!-- Check if user has test card -->
-                    @if(Auth::user()->member->listurl->type_id == 8)
-                        @php
-                            $date_member = date( 'Y-m-d', strtotime(Auth::user()->member->listurl->trial_date));
-                            $date_now = date( 'Y-m-d', strtotime(now()));
-                        @endphp
-                        <!-- Check if test card is expired -->
-                        @if($date_member > $date_now)
-                            <main id="main-container">
-                                @yield('content')
-                            </main>
-                        @else
-                            <main id="main-container">
-                                <div class="pt-5 ps-2">
-                                    <p class="mb-1">Your account has been expired. Please take contact with Swap to purchuse your own Swap digital business card.</p>
-                                    <a href="https://swap-nfc.be/help-center"><span class="text-muted">click here </span>>>> SWAP NFC <span> <<< </span></a>
-                                </div>
-                            </main>
+                    @if(Auth::user()->member->listurl)
+                        @if(Auth::user()->member->listurl->type_id == 8)
+                            @php
+                                $date_member = date( 'Y-m-d', strtotime(Auth::user()->member->listurl->trial_date));
+                                $date_now = date( 'Y-m-d', strtotime(now()));
+                            @endphp
+                            <!-- Check if test card is expired -->
+                            @if($date_member > $date_now)
+                                <main id="main-container">
+                                    @yield('content')
+                                </main>
+                            @else
+                                <main id="main-container">
+                                    <div class="pt-5 ps-2">
+                                        <p class="mb-1">Your account has been expired. Please take contact with Swap to purchuse your own Swap digital business card.</p>
+                                        <a href="https://swap-nfc.be/help-center"><span class="text-muted">click here </span>>>> SWAP NFC <span> <<< </span></a>
+                                    </div>
+                                </main>
+                        @endif
                     @endif
                     <!-- No test card -->
                     @else
